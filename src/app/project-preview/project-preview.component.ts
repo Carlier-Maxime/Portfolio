cc2b5b4c07131982672658ab40f4eca8b8f2217e
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-02-24 19:14:16 +0100

    project-preview : build HTML and setup var

--- src/app/project-preview/project-preview.component.ts
+++ src/app/project-preview/project-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-project-preview',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-preview.component.css']
 })
 export class ProjectPreviewComponent implements OnInit {
+  @Input() name : string = '';
+  @Input() description : string = '';
+  @Input() link : string = '';
+  @Input() img : string = '';
+  @Input() competences : string[] = [];
 
   constructor() { }
 
