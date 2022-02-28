704dfe49087a04622a0dc931cc01feb27f3c9b39
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-02-28 14:59:24 +0100

    project-view : add link for competence

--- src/app/project-preview/project-preview.component.ts
+++ src/app/project-preview/project-preview.component.ts
@@ -33,4 +33,22 @@ export class ProjectPreviewComponent implements OnInit {
   toggle(): void {
     this.isHover = !this.isHover;
   }
+
+  getLink(competence : string) : string {
+    switch (competence){
+      case "PHP" : return "https://www.php.net/";
+      case "Java" : return "https://www.oracle.com/fr/java/";
+      case "HTML" : return "https://html.spec.whatwg.org/multipage/";
+      case "CSS" : return "https://www.w3.org/Style/CSS/";
+      case "Angular" : return "https://angular.io/";
+      case "Typescript" : return "https://www.typescriptlang.org/";
+      case "Git" : return "https://git-scm.com/";
+      case "Java Swing" : return "https://docs.oracle.com/javase/6/docs/technotes/guides/swing/";
+      case "Laravel" : return "https://laravel.com/";
+      case "SQL" : return "https://sql.sh/";
+      case "Python" : return "https://www.python.org/";
+      case "PyQt" : return "https://wiki.python.org/moin/PyQt";
+      default: return "#";
+    }
+  }
 }
