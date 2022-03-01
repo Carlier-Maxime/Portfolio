04a3bae36a2ca0d6c5609ba0ded446a176243e63
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-03-01 12:17:56 +0100

    init routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProjectPreviewComponent } from './project-preview/project-preview.component';
 import {FormsModule} from "@angular/forms";
 import { CompetenceLinkComponent } from './competence-link/competence-link.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +19,16 @@ import { CompetenceLinkComponent } from './competence-link/competence-link.compo
     NavbarComponent,
     FooterComponent,
     ProjectPreviewComponent,
-    CompetenceLinkComponent
+    CompetenceLinkComponent,
+    AboutComponent,
+    ContactComponent,
+    HomepageComponent
   ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
+        AppRoutingModule,
     ],
   providers: [],
   bootstrap: [AppComponent]
