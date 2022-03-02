17ca076958828a67598be4ad26f19fba60076ea7
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-03-02 11:25:08 +0100

    add tab for competences

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  languages:string[] = ['Java','C++','Python','PHP','HTML','CSS','Typescript','SQL','Bash']
+  logiciels:string[] = ['Intellij IDEA','PhpStorm','PyCharm','WebStorm','Visual studio code','Visual studio','Notepad++','Unreal Engine','Git']
+  frameworks:string[] = ['Laravel','Angular','Qt','Pyqt','Java Swing','Bootstrap','Tensorflow','Keras']
+  se:string[] = ['Linux','Fedora','Windows 10','Windows 7']
 
   constructor() { }
 
